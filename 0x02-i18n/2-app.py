8b6b29f2343e9ff0410f8f46c90f6e7575bc58d9
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-11-20 11:23:56 +0000

    2 app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
-""" 2-app module """
-from flask import Flask, request
+"""Flask App"""
+from flask import Flask, render_template, request
 from flask_babel import Babel
-from routes.routes_2 import app_routes
-from config import Config
+
+
+class Config(object):
+    """Babel Config."""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-babel = Babel(app)
-
+app.debug = False
 app.config.from_object(Config)
-app.register_blueprint(app_routes)
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
-    """ Determine best match for supported languages
-    """
-    return request.accept_languages.best_match(Config.LANGUAGES)
+def get_locale():
+    """Select and return supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    """Handles route."""
+    return render_template("2-index.html")
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
