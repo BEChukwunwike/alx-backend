0237c387bddf422e8b79fa25eeed1bb792594cc0
BEChukwunwike <blessingechukwunwike@gmail.com>
2023-11-20 11:17:39 +0000

    1 app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-""" 1-app module """
+"""Flask App"""
 from flask import Flask, render_template
 from flask_babel import Babel
-from routes.routes_1 import app_routes
-
-
-app = Flask(__name__)
-babel = Babel(app)
 
 
 class Config(object):
-    """Config class for babel"""
+    """Babel  Config."""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+app.debug = False
 app.config.from_object(Config)
-app.register_blueprint(app_routes)
+babel = Babel(app)
+
 
 @app.route("/", strict_slashes=False)
 def index() -> str:
     """Handles route."""
     return render_template("1-index.html")
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
